e731c8971ca865a98556ee946fde5ed319af571b
Greg Keeley <GregoryKeeley@pursuit.org>
2020-08-31 21:00:53 -0400

    - Added tableview and extensions to main view controller

--- BirdFlower/ViewController.swift
+++ BirdFlower/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
 }
-
+extension ViewController: UITableViewDelegate {
+    
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+        // Common name
+        cell.textLabel?.text = ""
+        // Scinetific name
+        cell.detailTextLabel?.text = ""
+        return cell
+    }
+    
+    
+}
