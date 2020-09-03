4107ec1d754df6ed22276309e70a907e561e885e
Greg Keeley <GregoryKeeley@pursuit.org>
2020-09-03 19:19:30 -0400

    - changed color of some UI elements to match rest of the app

--- BirdFlower/View Controllers/ListViewController.swift
+++ BirdFlower/View Controllers/ListViewController.swift
@@ -50,6 +50,7 @@ class ListViewController: UIViewController {
         tableView.delegate = self
         loadAllData()
         persistenceDelegate = self
+        navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
     }
     override func viewWillAppear(_ animated: Bool) {
         fetchFavoriteDuos()
--- BirdFlower/View Controllers/MainTabBarController.swift
+++ BirdFlower/View Controllers/MainTabBarController.swift
@@ -17,6 +17,7 @@ class MainTabBarController: UITabBarController {
     //MARK:- View LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        UITabBar.appearance().tintColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
     }
     
 
