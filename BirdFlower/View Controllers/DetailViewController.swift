ff721f8f745421af9e71aea21e0bdaaf34744b5a
Greg Keeley <GregoryKeeley@pursuit.org>
2020-09-06 14:12:27 -0400

    - Created detail view for any item

---
+++ BirdFlower/View Controllers/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  BirdFlower
+//
+//  Created by Gregory Keeley on 9/6/20.
+//  Copyright © 2020 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
